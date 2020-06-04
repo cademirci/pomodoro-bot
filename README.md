59d9a5dedb2db67595b565324a87439388d46c3b
Çağlayan Demirci <demircicaglayan13@gmail.com>
2020-06-04 16:51:28 +0300

    initial commit / upload from computer

---
+++ README.md
@@ -0,0 +1,15 @@
+# Pomodoro Discord Bot
+
+A Pomodoro timing bot for Discord users. Pomodoro is a studying technique which recommends working in 25 and 5 minutes intervals. This bot warns the user by sending messages periodically according to Pomodoro algorithm.
+
+In order for me to provide a valid usage, you have to have a Discord developer account and create a bot. I cannot provide a Discord server that can execute Node for the bot. Take a developer key and open `bot.js`, uncomment the line `// const developerKey = YOUR_KEY;`, then change it properly.
+
+Node modules is also in the repository, you can just run below after having `npm` and `nodejs`.
+
+```
+node bot.js
+```
+
+You are welcome to submit corrections and additions.
+
+More information can be found in my [personal web page post](https://cademirci.com/a-discord-bot-and-javascript-timing-events).
---
+++ bot.js
@@ -0,0 +1,84 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+// const developerKey = YOUR_KEY;
+
+const prefix = '-pom';
+const second = 1000;
+const minute = 60 * second;
+const hour = 60 * minute;
+
+bot.on('ready', () => {
+    console.log(`bot is online as ${bot.user.tag}`);
+})
+
+bot.on('message', async message => {
+    var args = message.content.substring(prefix.length).split(' ');
+    var command1 = args[1];
+
+    if (message.content.substring(0, prefix.length) == prefix) {
+        if (command1 == 'start') {
+            var intervalCount = 0, interval = 0;
+            message.reply('One brain is started.');
+            setPart(interval);
+
+            // countdown clock for displaying
+            var remainingTime = 25, remainingCount = 1, status = 'working';
+            var countdown = await message.channel.send(`started! ${remainingTime} minutes`);
+            let clock = setInterval(() => {
+                remainingTime--;
+                if (remainingTime == 1)
+                    remainingCount++;
+                countdown.edit(`${remainingTime} minutes remain. :: ${status} ::`);
+                if (remainingCount == 10) {
+                    clearInterval(clock);
+                }
+                if (remainingTime == 0 && remainingCount % 2 == 0) {
+                    status = 'timeout';
+                    remainingTime += 5;
+                }
+
+                // looks like dirty code but this works instead of
+                // else { status = 'working' if ... } instead.
+                else if (remainingTime == 0 && remainingCount == 9) {
+                    remainingTime += 20;
+                    status = 'working';
+                }
+                else if (remainingTime == 0) {
+                    remainingTime += 25;
+                    status = 'working';
+                }
+            }, 1 * minute);
+
+            // pomodoro timer
+            function setPart(interval) {
+                intervalCount++;
+                interval = 25;
+                if (intervalCount % 2 == 0) interval = 5;
+                if (intervalCount == 9)     interval = 20;
+
+                let timer = setTimeout(() => {
+                    message.channel.send(`count ${intervalCount}, ${interval} minutes ended.`);
+                    setPart();
+                }, interval * minute);
+                if (intervalCount >= 10) {
+                    message.reply('The brain is ended.');
+                    clearTimeout(timer);
+                    return;
+                }
+            }
+        }
+        // I could not find a healthy way yet.
+        // I can try bot.commands.get('start').execute(message, args)
+        // for dynamic commands, later.
+        if (command1 == 'stop') {
+            // message.channel.send('pomodoro stopped.');
+        }
+        if (command1 == 'help') {
+            message.channel.send("-pom start: starts a pomodoro brain { (25min+5min)*4 + 20min }");
+        }
+    }
+
+})
+
+bot.login('${developerKey}');
